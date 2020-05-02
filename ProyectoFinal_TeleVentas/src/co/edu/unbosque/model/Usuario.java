c37f96d5bcac3f705f0419cd8fadf599c058a73e
tigre <tigre@192.168.56.1>
2020-05-02 11:29:25 -0500

    start

---
+++ ProyectoFinal_TeleVentas/src/co/edu/unbosque/model/Usuario.java
@@ -0,0 +1,100 @@
+package co.edu.unbosque.model;
+
+import javax.faces.bean.ManagedBean;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+@ManagedBean
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	
+	private int id;
+	private String usuario;
+	private String contraseña;
+	private String entidadbancaria;
+	private String fechatarjeta;
+	private String nombrereal;
+	
+	public Usuario() {
+		
+	}
+	//--------------------Getters y Setters--------------------//
+	
+	public String getFechatarjeta() {
+		return fechatarjeta;
+	}
+
+	public void setFechatarjeta(String fechatarjeta) {
+		this.fechatarjeta = fechatarjeta;
+	}
+
+	public String getNombrereal() {
+		return nombrereal;
+	}
+
+	public void setNombrereal(String nombrereal) {
+		this.nombrereal = nombrereal;
+	}
+
+		
+	public String getEntidadbancaria() {
+		return entidadbancaria;
+	}
+
+	public void setEntidadbancaria(String entidadbancaria) {
+		this.entidadbancaria = entidadbancaria;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+		
+	//--------------------------Métodos---------------------------//
+		
+	public String registro() {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ProyectoFinal_TeleVentas");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+			
+		Usuario u= new Usuario();
+		
+		// Guarda los datos ingresados en LoginUsuario a la base de datos
+		u.setUsuario(usuario);
+		u.setContraseña(contraseña);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(u);
+		entityManager.getTransaction().commit();
+
+		entityManager.close();
+		entityManagerFactory.close();
+			
+		return null;
+		
+	}
+		
+}
